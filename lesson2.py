be4b2336549c3ac1d9260f85bef1f5a28c042e69
Julian <moshammer.ju@gmail.com>
2020-03-11 21:10:56 +0100

    lesson2 further changes

--- lesson2.py
+++ lesson2.py
@@ -12,3 +12,12 @@ print(y)
 age = 18
 txt = "My name is Julian, and I am {0} years old"
 print(txt.format(age))
+
+a = "Python is the most awesome programming language"
+partOne = a[a.find("the"):50]
+partOne = partOne.replace(partOne[0:1], partOne[0:1].upper())
+partTwo = a[a.find("is"):a.find("the")-1]
+partThree = a[a.find("Python"):a.find("is")-1]
+result = "{} {} {}, even in 2020."
+print(result.format(partOne, partTwo, partThree))
+
